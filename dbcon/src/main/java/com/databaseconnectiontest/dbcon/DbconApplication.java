23c7fc7d2403b41ed72e1acc56e248ca9998cb70
Classic-username <josh.drapergame@gmail.com>
2023-10-12 15:38:10 -0600

    committing spring initializr files

---
+++ dbcon/src/main/java/com/databaseconnectiontest/dbcon/DbconApplication.java
@@ -0,0 +1,13 @@
+package com.databaseconnectiontest.dbcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbconApplication.class, args);
+	}
+
+}
